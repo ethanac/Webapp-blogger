71ab25e70bf551adfc46b80201832c6f063e4a4e
Ethan <haoz86@gmail.com>
2017-06-09 22:17:39 -0400

    add post method and some functions

--- www/coroweb.py
+++ www/coroweb.py
@@ -23,6 +23,37 @@ def get(path):
     return decorator()
 
 
+def post(path):
+    '''
+    Define decorator @post('/path')
+    '''
+    def decorator(func):
+        def wrapper(*args, **kw):
+            return func(*args, **kw)
+        wrapper.__method__ = 'POST'
+        wrapper.__route__ = path
+        return wrapper()
+    return decorator()
+
+
+def get_required_kw_args(fn):
+    args = []
+    params = inspect.signature(fn).parameters
+    for name, param in params.items():
+        if param.kind == inspect.Parameter.KEYWORD_ONLY and param.default == inspect.Parameter.empty:
+            args.append(name)
+    return tuple(args)
+
+
+def get_name_kw_args(fn):
+    args = []
+    params = inspect.signature(fn).parameters
+    for name, param in params.items():
+        if param.kind == inspect.Parameter.KEYWORD_ONLY:
+            args.append(name)
+    return tuple(args)
+
+
 class RequestHandler(object):
     def __init__(self, app, fn):
         self._app = app
