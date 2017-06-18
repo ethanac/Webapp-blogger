5a729da2d024103b5a491f860d9d31867b63c7ad
Ethan <haoz86@gmail.com>
2017-06-18 12:15:36 -0400

    add cookie2user

--- www/handlers.py
+++ www/handlers.py
@@ -27,6 +27,38 @@ def user2cookie(user, max_age):
     return '-'.join(l)
 
 
+@asyncio.coroutine
+def cookie2user(cookie_str):
+    '''
+    Parse cookie and load user if cookie is valid.
+    '''
+    if not cookie_str:
+        return None
+    try:
+        l = cookie_str.split('-')
+        if len(l) != 3:
+            return None
+        uid, expires, sha1 = l
+        if int(expires) < time.time():
+            return None
+        user = yield from User.find(uid)
+        if user is None:
+            return None
+        s = '%s-%s-%s-%s' % (uid, user.passwd, expires, _COOKIE_KEY)
+        if sha1 != hashlib.sha1(s.encode('utf-8')).hexdigest():
+            logging.info('invalid sha1')
+            return None
+        user.passwd = '******'
+        return user
+    except Exception as e:
+        logging.exception(e)
+        return None
+
+
+@asyncio.coroutine
+
+
+
 @get('/')
 def index(request):
     # users = yield from User.find_all()
@@ -48,6 +80,32 @@ def register():
     return {
         '__template__': 'register.html'
     }
+
+
+@post('/api/authenticate')
+def authenticate(*, email, passwd):
+    if not email:
+        raise APIValueError('email', 'Invalid email')
+    if not passwd:
+        raise APIValueError('passwd', 'Invalid password')
+    users = yield from User.find_all('email=?', [email])
+    if len(users) == 0:
+        raise APIValueError('email', 'Email not exist.')
+    user = users[0]
+    # check password
+    sha1 = hashlib.sha1()
+    sha1.update(user.id.encode('utf-8'))
+    sha1.update(b':')
+    sha1.update(passwd.encode('utf-8'))
+    if user.passwd != sha1.hexdigest():
+        raise APIValueError('passwd', 'Invalid password.')
+    # set cookie
+    r = web.Response()
+    r.set_cookie(COOKIE_NAME, user2cookie(user, 86400), max_age=86400, httponly=True)
+    user.passwd = '******'
+    r.content_type = 'application/json'
+    r.body = json.dump(user, ensure_ascii=False).encode('utf-8')
+    return r
 
 
 @post('/api/users')
